4d45d830c7254285cc286b15072f7fe5ee49e6dc
andreybyrsa <byrsa2004@bk.ru>
2022-10-31 07:42:27 +0500

    Поправил положение 3д модели в IntroSection

--- src/components/Models/BluePill.jsx
+++ src/components/Models/BluePill.jsx
@@ -12,7 +12,7 @@ export function BluePill(props) {
 
   return (
     <group ref={group} {...props} dispose={null}>
-      <mesh geometry={nodes.WhitePill.geometry} material={materials['Material.005']} position={[0, 0, 0]} rotation={[-Math.PI, 0.17, -Math.PI]} scale={0.75}>
+      <mesh geometry={nodes.WhitePill.geometry} material={materials['Material.005']} position={[0, .18, 0]} rotation={[-Math.PI, 0.17, -Math.PI]} scale={0.75}>
         <meshStandardMaterial color="#2f55d4"/>
         <mesh geometry={nodes.MovingBlock1.geometry} material={materials['Material.004']} position={[0, 1.94, -0.02]} rotation={[-2.68, -0.87, 1.99]} scale={0.83}>
           <meshStandardMaterial color="#888888"/>
--- src/components/Models/WhitePill.jsx
+++ src/components/Models/WhitePill.jsx
@@ -12,7 +12,7 @@ export function WhitePill(props) {
 
   return (
     <group ref={group} {...props} dispose={null}>
-      <mesh geometry={nodes.WhitePill.geometry} material={materials['Material.005']} position={[0, 0, 0]} rotation={[-Math.PI, 0.17, -Math.PI]} scale={0.75}>
+      <mesh geometry={nodes.WhitePill.geometry} material={materials['Material.005']} position={[0, .18, 0]} rotation={[-Math.PI, 0.17, -Math.PI]} scale={0.75}>
         <meshStandardMaterial color="white"/>
         <mesh geometry={nodes.MovingBlock1.geometry} material={materials['Material.004']} position={[0, 1.94, -0.02]} rotation={[-2.68, -0.87, 1.99]} scale={0.83}>
           <meshStandardMaterial color="#888888"/>
